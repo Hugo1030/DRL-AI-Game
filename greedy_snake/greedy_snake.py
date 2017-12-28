8bd2b9c65da11c3325481214a1c297a6ac2c580b
Hugo1030 <yzhjsw520@gmail.com>
2017-12-28 11:45:58 +0800

    greedy_snake update snake body and refreshing

--- greedy_snake/greedy_snake.py
+++ greedy_snake/greedy_snake.py
@@ -3,22 +3,32 @@
 import pygame
 import random
 
+WHITE = (0xff, 0xff, 0xff)
+BLACK = (0, 0, 0)
+GREEN = (0, 0xff, 0)
+RED = (0xff, 0, 0)
+LINE_COLOR = (0x33, 0x33, 0x33)
+
+D_LEFT, D_RIGHT, D_UP, D_DOWN = 0, 1, 2, 3
+
 pygame.init()
 
 WIDTH, HEIGHT = 500, 500
-scrren = pygame.display.set_mode((WIDTH, HEIGHT))
+CUBE_WIDTH = 20
+GRID_WIDTH_NUM, GRID_HEIGHT_NUM = WIDTH / CUBE_WIDTH, HEIGHT / CUBE_WIDTH
 
-pygame.display.set_caption('GreedySnake')
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("GreedySnake")
+
+# refreshing init
 clock = pygame.time.Clock()
 FPS = 30
 
 running = True
-while running:
-    clock.tick(FPS)
+counter = 0
+direction = D_LEFT
 
-    for event in pygame.evnet.get():
-        print(event.type)
-
-        if event.type == pygame.QUIT:
-            running = False
-
+# add body to the tail
+snake_body = []
+snake_body.append((int(GRID_WIDTH_NUM / 2) * CUBE_WIDTH,
+                   int(GRID_HEIGHT_NUM / 2) * CUBE_WIDTH))
